41028e8a2cc04f07ae896092583b572a98da75b3
iancasyana16 <iancasyana@gmail.com>
2023-11-10 20:04:44 +0700

    homepage dari saya

---
+++ aplikasi-web-lana/resources/views/home/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main')
+{{-- @section('titile' $title); --}}
+@section('content')
+    <div class="container">
+    </div>
+@endsection
---
+++ aplikasi-web-lana/resources/views/layout/navbar.blade.php
@@ -0,0 +1,33 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <div class="container">
+            <a class="navbar-brand d-flex fs-5" href="#">Lana Beauty</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="d-flex fw-bold p-3 mb-2 fs-5">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-5">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Service</a>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Gallery</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <form class="d-flex gap-1 fs-5">
+                <a href="#" class="btn text-decoration-underline text-white">Login</a>
+                <a href="#" class="btn btn-light">Sign Up</a>
+            </form>
+        </div>
+        </div>
+    </nav>
