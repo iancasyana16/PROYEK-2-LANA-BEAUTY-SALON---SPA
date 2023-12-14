d19c40ec1067fa48bbe128ae8617108ecf4c5567
iancasyana16 <iancasyana@gmail.com>
2023-12-14 10:16:17 +0700

    eror handling

--- aplikasi-web-lana/resources/views/admin/layanan/index.blade.php
+++ aplikasi-web-lana/resources/views/admin/layanan/index.blade.php
@@ -1,61 +1,58 @@
 @extends('layout.mainadmin')
 @section('content')
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper p-2">
+    <div class="content-wrapper p-2" <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <a href="" class="btn btn-primary">Tambah Layanan Baru</a>
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <a href="" class="btn btn-primary">Tambah Layanan Baru</a>
+                    <div class="card-tools">
+                        <div class="input-group input-group-sm mt-1" style="width: 150px;">
+                            <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
-                        <div class="card-tools">
-                            <div class="input-group input-group-sm mt-1" style="width: 150px;">
-                                <input type="text" name="table_search" class="form-control float-right"
-                                    placeholder="Search">
-
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn btn-default">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                </div>
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fas fa-search"></i>
+                                </button>
                             </div>
                         </div>
                     </div>
-
-                    <!-- /.card-header -->
-                    <div class="card-body table-responsive p-0">
-                        <table class="table table-hover text-nowrap">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Foto</th>
-                                    <th>Nama Layanan</th>
-                                    <th>Deskripsi</th>
-                                    <th>Harga</th>
-                                    <th>Opsi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>
-                                        <a href="" class="btn btn-warning">Edit</a>
-                                        <a href="" class="btn btn-danger">Hapus</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
                 </div>
-                <!-- /.card -->
+
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Foto</th>
+                                <th>Nama Layanan</th>
+                                <th>Deskripsi</th>
+                                <th>Harga</th>
+                                <th>Opsi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="" class="btn btn-warning">Edit</a>
+                                    <a href="" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
+            <!-- /.card -->
         </div>
+    </div>
 
     </div>
 @endsection
